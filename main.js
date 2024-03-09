c01c4d7ec8a1a84958a9a9d9ccc1f0e9fd7aeddb
Oussama <Messamessus@yahoo.com>
2024-03-09 21:14:54 +0100

    Checkpoint n°2 Sorting and searching algorithms

---
+++ main.js
@@ -0,0 +1,32 @@
+function insertionSort(array) {
+    for(let i = 1; i < array.length; i++) {
+        let key = array[i];
+        let j = i - 1;
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+    return array;
+}
+
+
+
+
+
+
+
+
+my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(my_array)
+for i in range(1,n):
+    insert_index = i
+    current_value = my_array.pop(i)
+    for j in range(i-1, -1, -1):
+        if my_array[j] > current_value:
+            insert_index = j
+    my_array.insert(insert_index, current_value)
+
+print("Sorted array:", my_array)
